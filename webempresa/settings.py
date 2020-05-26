605610fade2dc6338f5ab7bad06b9d3b41411348
Cristina Castillo <cris_rockarnolla2299@hotmail.com>
2020-05-26 16:13:20 -0500

    Procesadores de Contexto

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'blog',
     'core',
     'services.apps.ServicesConfig',
+    'social.apps.SocialConfig',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social.processors.ctx_dict',
             ],
         },
     },
